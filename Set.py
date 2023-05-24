69f9024aef02e8124e264b136de757aaeb18ec0a
LKenji <lucaskamikawa@gmail.com>
2023-05-24 12:58:27 -0300

    ANIMACOES DO CHARECTER SEM PROBLEMASS

--- Set.py
+++ Set.py
@@ -152,6 +152,7 @@ class Character(pygame.sprite.Sprite):
         for stair in STAIRS:
             if self.rect.colliderect(stair.rect):
                 self.on_stair=True
+                self.image = CHARACTER_IMG_UP
                 self.velocity=0
                 self.rect.y+=self.velocity
                 if keys[pygame.K_UP] or keys[pygame.K_w]:
@@ -162,56 +163,66 @@ class Character(pygame.sprite.Sprite):
                     self.rect.y+=2
                     self.lastupdown_key = 'down'
                     self.on_ground = False
+                self.image = CHARACTER_IMG_DOWN
             else:
+                
                 on_stair = False
 
-
-        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            self.rect.x -= 3
-            self.image = pygame.image.load('imagens/sprite_mario_andando_esquerda.png').convert_alpha()
-            self.image = pygame.transform.scale(self.image, (CHARACTER_WIDTH, CHARACTER_HEIGHT))
-            self.last_key = 'left'
-        elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            self.rect.x += 3
-            self.image = pygame.image.load('imagens/sprite_mario_andando_direita.png').convert_alpha()
-            self.image = pygame.transform.scale(self.image, (CHARACTER_WIDTH, CHARACTER_HEIGHT))
-            self.last_key = 'right'
-        if self.is_jumping:
-            if self.last_key == 'right':
-                self.image = CHARACTER_JUMPING_IMG_RIGHT
-            elif self.last_key == 'left':
-                self.image = CHARACTER_JUMPING_IMG_LEFT
-
-        # IMAGEM SALTANDO
-        elif self.is_jumping:
-            if self.last_key == 'right':
-                self.image = CHARACTER_JUMPING_IMG_RIGHT
-            elif self.last_key == 'left':
-                self.image = CHARACTER_JUMPING_IMG_LEFT 
-        # IMAGEM PARADO
-        elif not (keys[pygame.K_LEFT] or keys[pygame.K_a] or keys[pygame.K_RIGHT] or keys[pygame.K_d]):
-            if not self.on_ground:
-                # IMAGEM SALTANDO enquatoparado
-                if self.is_jumping:
-                    if self.last_key == 'right':
-                        self.image = CHARACTER_JUMPING_IMG_RIGHT
-                    elif self.last_key == 'left':
-                        self.image = CHARACTER_JUMPING_IMG_LEFT
-                # Imagem escada
-                if self.lastupdown_key == 'up' :
-                    self.image = CHARACTER_IMG_UP
-                elif self.lastupdown_key == 'down':
-                    self.image = CHARACTER_IMG_DOWN
-            elif self.on_ground and not self.on_stair:
+        if self.on_stair:
+            # Imagem escada
+            if self.lastupdown_key == 'up' :
+                self.image = CHARACTER_IMG_UP
+            elif self.lastupdown_key == 'down':
+                self.image = CHARACTER_IMG_DOWN
+            if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+                self.rect.x -= 3
+                self.last_key = 'left'
+            elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+                self.rect.x += 3
+                self.last_key = 'right'
+        else:
+            if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+                self.rect.x -= 3
+                self.image = pygame.image.load('imagens/sprite_mario_andando_esquerda.png').convert_alpha()
+                self.image = pygame.transform.scale(self.image, (CHARACTER_WIDTH, CHARACTER_HEIGHT))
+                self.last_key = 'left'
+            elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+                self.rect.x += 3
+                self.image = pygame.image.load('imagens/sprite_mario_andando_direita.png').convert_alpha()
+                self.image = pygame.transform.scale(self.image, (CHARACTER_WIDTH, CHARACTER_HEIGHT))
+                self.last_key = 'right'
+            # IMAGEM SALTANDO
+            if self.is_jumping: 
                 if self.last_key == 'right':
-                    self.image = CHARACTER_STAND_IMG_RIGHT
+                    self.image = CHARACTER_JUMPING_IMG_RIGHT
                 elif self.last_key == 'left':
-                    self.image = CHARACTER_STAND_IMG_LEFT
+                    self.image = CHARACTER_JUMPING_IMG_LEFT 
+            # IMAGEM PARADO
+            if not (keys[pygame.K_LEFT] or keys[pygame.K_a] or keys[pygame.K_RIGHT] or keys[pygame.K_d]):
+                if not self.on_ground:
+                    # IMAGEM SALTANDO enquatoparado
+                    if not self.on_stair:
+                        if self.last_key == 'right':
+                            self.image = CHARACTER_JUMPING_IMG_RIGHT
+                        elif self.last_key == 'left':
+                            self.image = CHARACTER_JUMPING_IMG_LEFT
+                    # Imagem escada
+                    if self.lastupdown_key == 'up' :
+                        self.image = CHARACTER_IMG_UP
+                    elif self.lastupdown_key == 'down':
+                        self.image = CHARACTER_IMG_DOWN
+                elif self.on_ground:
+                    if self.last_key == 'right':
+                        self.image = CHARACTER_STAND_IMG_RIGHT
+                    elif self.last_key == 'left':
+                        self.image = CHARACTER_STAND_IMG_LEFT
+            
         if on_stair==False:
             self.velocity += gravity
             self.rect.y += self.velocity
             self.lastupdown_key = 'still'
             self.on_ground = True
+            self.on_stair =False
 
         for platform in PLATFORMS:
             if self.rect.colliderect(platform.rect):
@@ -355,8 +366,8 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if game_over:
                 reset_game()
-            elif event.key == pygame.K_SPACE: 
-                character.jump()
+            elif event.key == pygame.K_SPACE:  
+                    character.jump()
                 
  
 
